9d2dcbba0739593c8e81aa6ea957420846982af2
Antoine Duperron <antoine.duperron@etu.univ-st-etienne.fr>
2022-03-29 12:27:29 +0200

    on commence mais a toi pages

--- Projet_CIP/Projet_CIP/Main.c
+++ Projet_CIP/Projet_CIP/Main.c
@@ -3,7 +3,9 @@
 #include <string.h>
 #include <float.h>
 #include <math.h>
-#include<time.h>
+#include <time.h>
+#include <cstdio> 
+
 
 #include "LibOutils.h"
 #include "libCIPSI1.h"
@@ -12,7 +14,15 @@
 
 void main(void) {
 
+	int nb_img_select = 0;
 
 
+	printf("\nSelectionnez le nombre d'image à traiter"); scanf("%d", &nb_img_select);
+	
+	for (int i = 0; i < nb_img_select; i++ )
+	{
+	
+	
+	}
 
 }
